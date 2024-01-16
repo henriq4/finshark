5ac33d8bf1d337cbd56acb08416866af1d2f254f
Henrique GC <henriq4net@gmail.com>
2024-01-16 10:48:06 -0300

    feat: create stock request dto

---
+++ DTOs/Stock/CreateStockRequestDTO.cs
@@ -0,0 +1,11 @@
+namespace finshark.DTOs.Stock;
+
+public class CreateStockRequestDTO
+{
+    public string Symbol { get; set; } = string.Empty;
+    public string Company { get; set; } = string.Empty;
+    public decimal Purchase { get; set; }
+    public decimal LastDiv { get; set; }
+    public string Industry { get; set; } = string.Empty;
+    public long MarketCap { get; set; }
+}
