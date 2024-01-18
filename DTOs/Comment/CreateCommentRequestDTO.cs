7413c09bcb3f104c44850fe6a1c266be6e33babb
Henrique GC <henriq4net@gmail.com>
2024-01-18 10:16:44 -0300

    feat: dto for create comment request

---
+++ DTOs/Comment/CreateCommentRequestDTO.cs
@@ -0,0 +1,7 @@
+namespace finshark.DTOs.Comment;
+
+public class CreateCommentRequestDTO
+{
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+}
