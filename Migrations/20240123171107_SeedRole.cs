55df60dd9e68479cbd18c8ed36ab3ef559062ace
Henrique GC <henriq4net@gmail.com>
2024-01-24 10:03:44 -0300

    feat: add user roles

---
+++ Migrations/20240123171107_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace finshark.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "15fc7f30-37c8-47cf-8778-7ea3249c2efe", null, "User", "USER" },
+                    { "a6352ae7-fbec-4bcf-a352-f20dd0835066", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "15fc7f30-37c8-47cf-8778-7ea3249c2efe");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a6352ae7-fbec-4bcf-a352-f20dd0835066");
+        }
+    }
+}
