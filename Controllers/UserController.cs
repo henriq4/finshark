d20ae73809947095492ad83cee406df2885d2ed1
Henrique GC <henriq4net@gmail.com>
2024-01-24 10:47:50 -0300

    feat: create user login endpoint

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,6 +3,7 @@ using finshark.Interfaces;
 using finshark.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace finshark.Controllers;
 
@@ -12,14 +13,16 @@ public class UserController : ControllerBase
 {
     private readonly UserManager<User> _userManager;
     private readonly ITokenService _tokenService;
+    private readonly SignInManager<User> _signInManager;
 
-    public UserController(UserManager<User> userManager, ITokenService tokenService)
+    public UserController(UserManager<User> userManager, ITokenService tokenService, SignInManager<User> signInManager)
     {
         this._userManager = userManager;
         _tokenService = tokenService;
+        _signInManager = signInManager;
     }
 
-    [HttpPost]
+    [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] CreateUserRequestDTO userDTO)
     {
         try
@@ -51,5 +54,26 @@ public class UserController : ControllerBase
         {
             return StatusCode(500, e);
         }     
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestDTO userDTO)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        var user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == userDTO.Username);
+
+        if (user == null) return Unauthorized("Invalid username or password");
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, userDTO.Password, false);
+
+        if (!result.Succeeded) return Unauthorized("Invalid username or password");
+
+        return Ok(new LoginResponseDTO
+        {
+            Email = user.Email,
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        });
     }
 }
